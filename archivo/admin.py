6cf9085a8085919f9607194f940970c6ecfa4c60
Matias Benedetto <matias.benedetto@gmail.com>
2013-11-27 00:20:25 -0300

    modificando maquetado

--- archivo/admin.py
+++ archivo/admin.py
@@ -4,6 +4,7 @@ from models import *
 admin.site.register(Fuerza)
 admin.site.register(Circunstancia)
 admin.site.register(Archivodecasos)
+admin.site.register(TipoEdad)
 
 class CasoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'apellido', 'edad', 'sexo', 'provincia', 'fecha_deceso')
