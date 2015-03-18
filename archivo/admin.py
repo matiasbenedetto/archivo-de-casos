be8e95232c99ebd93bbf68d0cb76bea2cbaca904
Matias Benedetto <matias.benedetto@gmail.com>
2015-03-18 14:55:50 -0300

    agrego campo imagen y hago cambios en el admin

--- archivo/admin.py
+++ archivo/admin.py
@@ -7,6 +7,10 @@ admin.site.register(Archivodecasos)
 admin.site.register(TipoEdad)
 
 class CasoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'apellido', 'edad', 'sexo', 'provincia', 'fecha_deceso')
-    list_editable = ('sexo',) 
+	list_display_links=('nombre', 'apellido')
+	list_display = ('foto', 'id', 'nombre', 'apellido', 'edad', 'sexo', 'provincia', 'fecha_deceso')
+	list_editable = ('sexo',)
+	search_fields = ['nombre', 'apellido', 'edad', 'provincia']
+	list_filter = ['sexo', 'provincia', 'circunstancia', 'fuerza']
+
 admin.site.register(Caso, CasoAdmin)
